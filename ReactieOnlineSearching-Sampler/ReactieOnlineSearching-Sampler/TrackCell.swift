89526e1d13fd99430a44545c7095c6d689b94eeb
Kohei Arai <a_la_mode1108@icloud.com>
2018-10-20 19:39:06 +0900

    added trackcell

---
+++ ReactieOnlineSearching-Sampler/ReactieOnlineSearching-Sampler/TrackCell.swift
@@ -0,0 +1,16 @@
+//
+//  TrackCell.swift
+//  ReactieOnlineSearching-Sampler
+//
+//  Created by Kohei Arai on 2018/10/20.
+//  Copyright © 2018年 Kohei Arai. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    
+}
